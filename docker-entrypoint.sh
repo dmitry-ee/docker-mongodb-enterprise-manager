377951b9b37e1889a998e0dadf1f2bf0906f1369
Dmitry Evdokimov <devdokimov@alfabank.ru>
2018-09-15 02:27:25 +0300

    feature: auto-copy original conf files without overriding

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 if [ "$1" = 'mongodb-mms' -a "$(id -u)" = '0' ]; then
+  cp -n $MONGO_ENTERPRISE_MANAGER_OCONF_DIR/* $MONGO_ENTERPRISE_MANAGER_CONF_DIR/*
   /usr/bin/supervisord
   while true; do
     sleep 60
