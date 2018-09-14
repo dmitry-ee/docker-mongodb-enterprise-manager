9117a60b944f232b7c243d1b436e03a1321f9dd6
Dmitry Evdokimov <devdokimov@alfabank.ru>
2018-09-15 01:32:43 +0300

    fix: infinite loop

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,12 @@ sigterm_handler() {
   exec "mongodb-mms stop"
 }
 
-if [ "$1" = 'ops-manager' -a "$(id -u)" = '0' ]; then
+if [ "$1" = 'mongodb-mms' -a "$(id -u)" = '0' ]; then
   exec "mongodb-mms start"
+  while true; do
+    echo "$(mongodb-mms status)"
+    sleep 60
+  done
 fi
 
 trap 'sigterm_handler' SIGTERM
