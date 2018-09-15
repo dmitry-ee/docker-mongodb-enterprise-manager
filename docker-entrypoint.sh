e99273853fbbcf5bbe45d94efa424ab3c068a0ae
Dmitry Evdokimov <devdokimov@alfabank.ru>
2018-09-15 03:20:53 +0300

    fix: -r

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = 'mongodb-mms' -a "$(id -u)" = '0' ]; then
-  cp -n $MONGO_ENTERPRISE_MANAGER_OCONF_DIR* $MONGO_ENTERPRISE_MANAGER_CONF_DIR*
+  cp -n -r $MONGO_ENTERPRISE_MANAGER_OCONF_DIR $MONGO_ENTERPRISE_MANAGER_CONF_DIR
   gosu "/usr/bin/supervisord"
   while true; do
     sleep 60
