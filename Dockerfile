d903ccd7c0bc49e7fee453c26e7aa0b1447805bb
Dmitry <devdokimoff@gmail.com>
2018-09-15 15:54:30 +0300

    feature: final version withoug gosu & supervisor

--- Dockerfile
+++ Dockerfile
@@ -9,16 +9,7 @@ ENV 				GOSU_VERSION 1.7
 RUN 				set -x \
 						&& apt-get update \
 						&& apt-get install -y --no-install-recommends \
-							ca-certificates wget bash openssl supervisor \
-						&& rm -rf /var/lib/apt/lists/* \
-						&& wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture)" \
-						&& wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture).asc" \
-						&& export GNUPGHOME="$(mktemp -d)" \
-						&& gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 \
-						&& gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu \
-						&& rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc \
-						&& chmod +x /usr/local/bin/gosu \
-						&& gosu nobody true \
+							ca-certificates wget bash openssl \
 						&& rm -rf /var/lib/apt/lists/*
 
 # MONGO_ENTERPRISE_MANAGER_BUILD could be obtained from https://www.mongodb.com/download-center/ops-manager/releases
@@ -64,4 +55,4 @@ ENV         PATH=/opt/mongodb/mms/bin/:$PATH
 
 EXPOSE 			8080
 
-CMD 				[ "mongodb-mms" ]
+CMD 				[ "mongodb-mms", "backup-daemon" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'mongodb-mms' -a "$(id -u)" = '0' ]; then
+export TERM="xterm"
 
-  cp -n -r  $MONGO_ENTERPRISE_MANAGER_CONF_ORIG_DIR/. $MONGO_ENTERPRISE_MANAGER_CONF_DIR
+stop_mongodb_mms()
+{
+    echo "Stopping MongoDB Ops Manager [Daemon]..."
+    /opt/mongodb/mms/bin/mongodb-mms stop
+}
+stop_mongodb_mms_backup()
+{
+    echo "Stopping MongoDB Ops Manager [Backup Daemon]..."
+    /opt/mongodb/mms/bin/mongodb-mms stop
+}
+
+if [ "$1" = 'mongodb-mms' ]; then
 
   chown -R  $MONGO_ENTERPRISE_MANAGER_USER:$MONGO_ENTERPRISE_MANAGER_USER \
             $MONGO_ENTERPRISE_MANAGER_CONF_DIR \
             $MONGO_ENTERPRISE_MANAGER_CERT_DIR \
             $MONGO_ENTERPRISE_MANAGER_LOG_DIR
 
-  groupadd supervisor && usermod -a $MONGO_ENTERPRISE_MANAGER_USER -G supervisor
+  cp -n -r  $MONGO_ENTERPRISE_MANAGER_CONF_ORIG_DIR/. $MONGO_ENTERPRISE_MANAGER_CONF_DIR
 
-  gosu $MONGO_ENTERPRISE_MANAGER_USER supervisord
+  trap stop_mongodb_mms HUP INT QUIT KILL TERM
 
-  # while true; do
-  #   sleep 60
-  # done
+  echo "MongoDB Ops Manager [Daemon] is starting..."
+  mongodb-mms start
+  echo "MongoDB Ops Manager [Daemon] is running"
+
+  while true; do
+    sleep 1000
+  done
 
 fi
+
+if [ "$1" = 'backup-daemon' ]; then
+
+  trap stop_mongodb_mms_backup HUP INT QUIT KILL TERM
+
+  echo "MongoDB Ops Manager [Backup Daemon] is starting..."
+  mongodb-mms-backup-daemon start
+  echo "MongoDB Ops Manager [Backup Daemon] is running"
+
+fi
+
+
 
 exec "$@"
