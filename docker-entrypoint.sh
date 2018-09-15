59600f1306cfdd073c369fba2444a3e2c61d7a1f
Dmitry Evdokimov <devdokimov@alfabank.ru>
2018-09-15 05:05:52 +0300

    fix: args -> envs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'mongodb-mms' -a "$(id -u)" = '0' ]; then
   chown -R  $MONGO_ENTERPRISE_MANAGER_CONF_DIR \
             $MONGO_ENTERPRISE_MANAGER_CERT_DIR \
             $MONGO_ENTERPRISE_MANAGER_LOG_DIR
-  cp -n -r  $MONGO_ENTERPRISE_MANAGER_OCONF_DIR/. $MONGO_ENTERPRISE_MANAGER_CONF_DIR
+  cp -n -r  $MONGO_ENTERPRISE_MANAGER_CONF_ORIG_DIR/. $MONGO_ENTERPRISE_MANAGER_CONF_DIR
   supervisord
   while true; do
     sleep 60
